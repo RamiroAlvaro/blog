586c09387c7dbbad79ab0247a67dcc89df0e0ce3
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-08-19 18:13:19 -0300

    Formulario de envio de email

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# E-mail
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
