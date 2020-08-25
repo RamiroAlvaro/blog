a5247bfc2a7e8b522a6e992861bd78d5cfdd450c
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-08-25 20:43:34 -0300

    Escribiendo el primer test

---
+++ mysite/blog/tests/test_post_list.py
@@ -0,0 +1,14 @@
+import pytest
+from django.urls import reverse
+
+from mysite.django_assertions import assert_equal
+
+
+@pytest.fixture
+def resp(client, db):
+    resp = client.get(reverse('blog:post_list'))
+    return resp
+
+
+def test_status_code(resp):
+    assert_equal(resp.status_code, 200)
